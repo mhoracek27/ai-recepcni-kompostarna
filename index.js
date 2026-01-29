13e216b5d801fe4dd5039da7ce5a4fc22135bf65
mhoracek27 <m.horacek27@gmail.com>
2026-01-30 00:58:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import fetch from "node-fetch";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
